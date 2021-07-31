73b5e3591b2747efe5eb04d742c803b4cc590435
talentdev2020 <polaris.center@outlook.com>
2021-07-31 08:27:38 -0700

    disable estlint for useEffect

--- src/components/Blocks.js
+++ src/components/Blocks.js
@@ -34,6 +34,7 @@ const Blocks = () => {
     
     useEffect(() => {
         getBlocks();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [])
 
     const getBlocks = async() => {
