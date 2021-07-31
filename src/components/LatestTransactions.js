4794e5f7f6d2765623abe71e00e0f952db953833
talentdev2020 <polaris.center@outlook.com>
2021-07-30 21:09:14 -0700

    get all transactions

--- src/components/LatestTransactions.js
+++ src/components/LatestTransactions.js
@@ -12,19 +12,29 @@ const LatestTransactions = () => {
         if(web3) {
             const latest = await web3.eth.getBlockNumber()
 
+            let promises = [];
             for (var i=0; i < 10; i++) {
                 const blockNumber = latest - i;
 
-                web3.eth.getBlock(blockNumber).then(data =>{
-                    console.log({data})
-                });
-              }
+                promises.push(new Promise((res, rej) => {
+                    web3.eth.getBlock(blockNumber, true).then(data =>{
+                        res(data);
+                    }); 
+                }));
+            }
+
+            return Promise.all(promises);
         }
+    }
+
+    const getTransactions = async() => {
+        const transanctions = await loginMetamask();
+        console.log({transanctions})
     }
 
     return (
          <div>
-            <button className='btnLogin' onClick={() => loginMetamask()}> Login</button>
+            <button className='btnLogin' onClick={() => getTransactions()}> Login</button>
         </div>
     )
 }
